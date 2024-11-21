6392f14491b9eec0e92c234ef4be91ab22934ad5
Michiel Dekker <33653504+chield@users.noreply.github.com>
2024-11-21 10:01:00 +0100

    Update win11.ps1

--- win11.ps1
+++ win11.ps1
@@ -1,28 +1,30 @@
-$ScriptName = 'Interstellar Windows 11'
-$ScriptVersion = '24.7.4.4'
-Write-Host -ForegroundColor Green "$ScriptName $ScriptVersion"
+#start-osd cloud
+write-host "last update date: 20/11/2024|14:13"
 
-<# Offline Driver Details
-If you extract Driver Packs to your Flash Drive, you can DISM them in while in WinPE and it will make the process much faster, plus ensure driver support for first Boot
-Extract to: OSDCLoudUSB:\OSDCloud\DriverPacks\DISM\$ComputerManufacturer\$ComputerProduct
-Use OSD Module to determine Vars
-$ComputerProduct = (Get-MyComputerProduct)
-$ComputerManufacturer = (Get-MyComputerManufacturer -Brief)
-#>
+#=======================================================================
+#   OSDCloud Development Branch notification
+#=======================================================================
 
+Write-Host -ForegroundColor Red -BackgroundColor Black "########################################################################################################"
+Write-Host -ForegroundColor Red -BackgroundColor Black "##                                                                                                    ##"
+Write-Host -ForegroundColor Red -BackgroundColor Black "##  This is the development branch of the OSDCloud process, please wait 20 seconds before continuing  ##"
+Write-Host -ForegroundColor Red -BackgroundColor Black "##                                                                                                    ##"
+Write-Host -ForegroundColor Red -BackgroundColor Black "########################################################################################################"
+Start-Sleep -Seconds 20
+clear-host
 
-
-#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+#=======================================================================
+#   OSDCLOUD Definitions
+#=======================================================================
 $Product = (Get-MyComputerProduct)
 $Model = (Get-MyComputerModel)
 $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $OSVersion = 'Windows 11' #Used to Determine Driver Pack
 $OSReleaseID = '23H2' #Used to Determine Driver Pack
-$OSName = 'Windows 11 23H2 x64'
+$OSName = "$OSVersion $OSReleaseID x64"
 $OSEdition = 'Pro'
 $OSActivation = 'Retail'
 $OSLanguage = 'en-us'
-
 
 #=======================================================================
 #   OSDCLOUD VARS
@@ -31,10 +33,10 @@ $Global:MyOSDCloud = [ordered]@{
     Restart               = [bool]$false
     RecoveryPartition     = [bool]$true
     OEMActivation         = [bool]$true 
-    WindowsUpdate         = [bool]$false #temporarily disabled same thing almost 10 minutes
-    MSCatalogFirmware     = [bool]$false #temporarily disabled no impact
-    WindowsUpdateDrivers  = [bool]$false #temporarily disabled this is causing long delays on the getting ready screen before the oobe (almost 10 minutes)
-    WindowsDefenderUpdate = [bool]$false #temporarily disabled same thing almost 10 minutes
+    WindowsUpdate         = [bool]$true #temporarily disabled same thing almost 10 minutes
+    MSCatalogFirmware     = [bool]$true #temporarily disabled no impact
+    WindowsUpdateDrivers  = [bool]$true #temporarily disabled this is causing long delays on the getting ready screen before the oobe (almost 10 minutes)
+    WindowsDefenderUpdate = [bool]$true #temporarily disabled same thing almost 10 minutes
     SetTimeZone           = [bool]$true
     SkipClearDisk         = [bool]$false
     ClearDiskConfirm      = [bool]$false
@@ -43,124 +45,83 @@ $Global:MyOSDCloud = [ordered]@{
     CheckSHA1             = [bool]$true
 }
 
-#Testing Custom Images - Use this if you want to automate using your own WIM / ESD file
-#Region Custom Image
-$WIMName = 'W11 - Nov 2024.wim'
-$ImageFileItem = Find-OSDCloudFile -Name $WIMName  -Path '\OSDCloud\OS\'
-if ($ImageFileItem){
-    $ImageFileItem = $ImageFileItem | Where-Object {$_.FullName -notlike "C*"} | Where-Object {$_.FullName -notlike "X*"} | Select-Object -First 1
-    if ($ImageFileItem){
-        $ImageFileName = Split-Path -Path $ImageFileItem.FullName -Leaf
-        $ImageFileFullName = $ImageFileItem.FullName
-        
-        $Global:MyOSDCloud.ImageFileItem = $ImageFileItem
-        $Global:MyOSDCloud.ImageFileName = $ImageFileName
-        $Global:MyOSDCloud.ImageFileFullName = $ImageFileFullName
-        $Global:MyOSDCloud.OSImageIndex = 6
+#for a more complete rollout of the OSDCloud process, you can enable the following options: WindowsUpdate, MSCatalogFirmware, WindowsUpdateDrivers, WindowsDefenderUpdate, SyncMSUpCatDriverUSB
+
+#=======================================================================
+#   GENERAL VARIABLES
+#=======================================================================
+
+#developermode asks for a keypress before rebooting
+$askbeforereboot = $true
+
+#=======================================================================
+#   LOCAL DRIVE LETTERS
+#=======================================================================
+function Get-WinPEDrive {
+    $WinPEDrive = (Get-WmiObject Win32_LogicalDisk | Where-Object { $_.VolumeName -eq 'WINPE' }).DeviceID
+    write-host "Current WINPE drive is: $WinPEDrive"
+    return $WinPEDrive
+}
+function Get-OSDCloudDrive {
+    $OSDCloudDrive = (Get-WmiObject Win32_LogicalDisk | Where-Object { $_.VolumeName -eq 'OSDCloudUSB' }).DeviceID
+    write-host "Current OSDCLOUD Drive is: $OSDCloudDrive"
+    return $OSDCloudDrive
+}
+
+#=======================================================================
+#   OSDCLOUD Image
+#=======================================================================
+$uselocalimage = $true
+#$WIMName = 'Windows 11 23H2 - okt.wim'
+$Windowsversion = "$OSVersion $OSReleaseID"
+$OSDCloudDrive = Get-OSDCloudDrive
+Write-Host -ForegroundColor Green -BackgroundColor Black "UseLocalImage is set to: $uselocalimage"
+#dynamically find the latest version based on the variables set in the beginning of the script
+if ($uselocalimage -eq $true) {
+    # Find the latest month WIM file
+    $months = @("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "okt", "nov", "dec")
+    $wimlist = Get-ChildItem -Path "$OSDCloudDrive\OSDCloud\OS\" -Filter "*.wim" -Recurse
+    write-host "Available wimfiles: $wimlist"
+    $wimFiles = Get-ChildItem -Path "$OSDCloudDrive\OSDCloud\OS\" -Filter "*.wim" -Recurse | Where-Object { $_.Name -match "$Windowsversion" }
+    $latestMonth = $months | Where-Object { $wimFiles.Name -match $_ } | Select-Object -Last 1
+
+    if ($latestMonth) {
+        $WIMName = "$Windowsversion - $latestMonth.wim"
+        Write-Host -ForegroundColor Green -BackgroundColor Black "Latest WIM file found: $WIMName This WimFile will be used for the installation"
+    }
+    else {
+        Write-Host -ForegroundColor Red -BackgroundColor Black "No WIM files found for $Windowsversion using esd as backup."
+        Write-Host -ForegroundColor Red -BackgroundColor Black "PLEASE ADD THE WIM FILE TO THE OSDCLOUD USB DRIVE"
+        $uselocalimage = $false
+        Start-Sleep -Seconds 10
     }
 }
-#endregion Custom Image
 
-#Testing MS Update Catalog Driver Sync
-#$Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+if ($uselocalimage -eq $true) {
+    $ImageFileItem = Find-OSDCloudFile -Name $WIMName  -Path "\OSDCloud\OS\"
+    if ($ImageFileItem) {
+        write-host "Variable uselocalimage is set to true. The installer will try to find and use the wim file called: $WIMName"
+        $ImageFileItem = $ImageFileItem | Where-Object { $_.FullName -notlike "C*" } | Where-Object { $_.FullName -notlike "X*" } | Select-Object -First 1
+        if ($ImageFileItem) {
+            $ImageFileName = Split-Path -Path $ImageFileItem.FullName -Leaf
+            $ImageFileFullName = $ImageFileItem.FullName
+            
+            $Global:MyOSDCloud.ImageFileItem = $ImageFileItem
+            $Global:MyOSDCloud.ImageFileName = $ImageFileName
+            $Global:MyOSDCloud.ImageFileFullName = $ImageFileFullName
+            $Global:MyOSDCloud.OSImageIndex = 3
+        }
+    }
+}
 
-#Used to Determine Driver Pack
+#=======================================================================
+#   Specific Driver Pack
+#=======================================================================
 $DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
 
-if ($DriverPack){
+if ($DriverPack) {
     $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
 }
-
-#***********************************
-# Updating Surface Driver Catalog
-#***********************************
-function Update-OSDCloudSurfaceDriverCatalogJustInTime {   
-    [CmdletBinding()]
-    param (
-        [switch]$UpdateDriverPacksJson,
-        [string[]]$ForceUpdateProducts
-    )
-
-    $ProgressPreference = 'SilentlyContinue'
-
-    function Get-NewDownloadCenterDriverPack {
-        param (
-            $DriverPack,
-            [switch]$UpdateDriverPacksJson
-        )
-        
-        try {
-            Write-Verbose "Download Center URL: $($DriverPack.DownloadCenter)"
-            $DownloadCenter = Invoke-WebRequest -Uri $DriverPack.DownloadCenter -UseBasicParsing | Select-Object -ExpandProperty Content
-
-            $DriverOsVersion = $DriverPack.Url -match '_(Win[\d]{2})_' | ForEach-Object { $Matches[1] }
-            $MsiDownloadsMatch = [regex]::Matches($DownloadCenter, '"url":"(https:[\w\-\/\.]+\.msi)"')
-            $UpdatedDriverUrl = ($MsiDownloadsMatch | Where-Object { $_.Value -match $DriverOsVersion }).Groups[1].Value
-            $UpdateDriverFilename = $UpdatedDriverUrl.Split('/') | Select-Object -Last 1
-
-            #Replace download links in CloudDriverPacks.json
-            if ($UpdateDriverPacksJson) {
-                $content = [System.IO.File]::ReadAllText($Script:LocalCloudDriverPacksJson)
-
-                Write-Verbose "Replacing [$($DriverPack.Url)] with [$($UpdatedDriverUrl)]"
-                $content = $content.Replace($DriverPack.Url, $UpdatedDriverUrl)
-
-                Write-Verbose "Replacing [$($DriverPack.FileName)] with [$($UpdateDriverFilename)]"
-                $content = $content.Replace($DriverPack.FileName, $UpdateDriverFilename)
-
-                [System.IO.File]::WriteAllText($Script:LocalCloudDriverPacksJson, $content)
-                
-                Write-Host "Updated download URL to: $($UpdatedDriverUrl)"
-            }
-            else {
-                Write-Host "New download URL: $($UpdatedDriverUrl)"
-            }
-        }
-        catch {
-            Write-Warning 'Unable to retrieve updated download URL'
-        }
-
-    }
-
-    $OSDModuleBase = (Get-Module -Name OSD -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).ModuleBase
-    $Script:LocalCloudDriverPacksJson = (Join-Path $OSDModuleBase 'Catalogs\CloudDriverPacks.json')
-    $LocalMicrosoftDriverPacksJson = (Join-Path $OSDModuleBase 'Catalogs\MicrosoftDriverPackCatalog.json')
-
-    $Catalog = Get-Content -Encoding UTF8 -Raw -Path $LocalMicrosoftDriverPacksJson | ConvertFrom-Json
-
-    foreach ($DriverPack in $Catalog) {
-        $Updated = $false
-        try {
-            $Response = Invoke-WebRequest -Method Head -Uri $DriverPack.Url -UseBasicParsing -ErrorAction Stop
-            Write-Verbose ('{0} {1}' -f $Response.StatusCode, $DriverPack.Url)
-        }
-        catch [System.Net.WebException] {
-            $Err = $_
-            Write-Warning "DriverPack for $($DriverPack.Name) ($($DriverPack.Product)) is not available at $($DriverPack.Url)"
-            Write-Verbose $Err.Exception.Message
-
-            $Updated = $true
-            Get-NewDownloadCenterDriverPack -DriverPack $DriverPack -UpdateDriverPacksJson:$UpdateDriverPacksJson
-        }
-        catch {
-            $Err = $_
-            Write-Warning "$($Err.Exception.Message) [$($Err.Exception.GetType().FullName)]"
-        }
-
-        if(($ForceUpdateProducts -contains $DriverPack.Product) -and (-NOT $Updated)) {
-            Write-Host "Forcing update for $($DriverPack.Product)"
-            Write-Warning "Updating DriverPack for $($DriverPack.Name) ($($DriverPack.Product)) at $($DriverPack.Url)"
-            Get-NewDownloadCenterDriverPack -DriverPack $DriverPack -UpdateDriverPacksJson:$UpdateDriverPacksJson
-        }
-    }
-}
-
-Write-Host "Updating Surface Driver Catalog..."
-#Invoke-RestMethod "https://raw.githubusercontent.com/chield/OSDCloud/refs/heads/main/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1" | Invoke-Expression
-Update-OSDCloudSurfaceDriverCatalogJustInTime -UpdateDriverPackJson
-
-#Enable HPIA | Update HP BIOS | Update HP TPM
-
 $UseHPIA = $true #temporarily disabled
 if ($Manufacturer -match "HP" -and $UseHPIA -eq $true) {
     #$Global:MyOSDCloud.DevMode = [bool]$True
@@ -168,27 +129,45 @@ if ($Manufacturer -match "HP" -and $UseHPIA -eq $true) {
     { $Global:MyOSDCloud.HPIAALL = [bool]$true }
     $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
     $Global:MyOSDCloud.HPCMSLDriverPackLatest = [bool]$true
-
 }
+
 if ($Manufacturer -match "HP") {
     install-module -Name HPCMSL -Force -AcceptLicense -Scope AllUsers -SkipPublisherCheck
 }
 
-
-#write variables to console
+#=======================================================================
+#   Write OSDCloud VARS to Console
+#=======================================================================
 Write-Output $Global:MyOSDCloud
 
-#Update Files in Module that have been updated since last PowerShell Gallery Build (Testing Only)
-$ModulePath = (Get-ChildItem -Path "$($Env:ProgramFiles)\WindowsPowerShell\Modules\osd" | Where-Object {$_.Attributes -match "Directory"} | select -Last 1).fullname
-import-module "$ModulePath\OSD.psd1" -Force
-
-#Launch OSDCloud
-Write-Host "Starting OSDCloud" -ForegroundColor Green
-write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
-
+#=======================================================================
+#   Start OSDCloud installation
+#=======================================================================
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
 
-write-host "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot" -ForegroundColor Green
+#=======================================================================
+#   OSDCLOUD Specialize Scripts and modules
+#=======================================================================
+$WINPEDrive = Get-WinPEDrive
+$localscriptsosdfolder = "$WINPEDrive\fblocalscripts"
+$localscriptfolderPath = "C:\Windows\Panther\fblocalscripts"
+if (-NOT (Test-Path "$localscriptfolderPath")) {
+    New-Item -Path "$localscriptfolderPath" -ItemType Directory -Force | Out-Null
+}
+Copy-Item -Path $localscriptsosdfolder\* -Destination $localscriptfolderPath\ -Recurse -Force
+install-module -Name AzureAD -Force -AcceptLicense -Scope AllUsers -SkipPublisherCheck
+install-module -Name WindowsAutopilotIntune -Force -AcceptLicense -Scope AllUsers -SkipPublisherCheck
 
-#Restart
-restart-computer
+
+#=======================================================================
+#   DEVELOPER USER CONFIRMATION TO REBOOT
+#=======================================================================
+if ($askbeforereboot -eq $true) {
+    Write-Host -ForegroundColor Yellow "Press any key to reboot the device"
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+#=======================================================================
+#   REBOOT DEVICE
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting now!"
+Restart-Computer -Force
